b48995d4ff97f9acd70526defff29ccec77ac601
AzSiAz <slider.perso@live.fr>
2016-08-07 18:06:21 +0200

    separate html template from Ctrl

--- app/components/PopoverChapterReader.ts
+++ app/components/PopoverChapterReader.ts
@@ -3,67 +3,7 @@ import {App, Popover, NavController, Content, NavParams, Toast, Platform, Storag
 
 
 @Component({
-  template: `
-    <ion-list radio-group [(ngModel)]="fontFamily" (ionChange)="changeFontFamily()" class="popover-page">
-      <ion-row>
-        <ion-col>
-          <button (click)="changeFontSize('small')" ion-item detail-none class="text-button text-smaller">A</button>
-        </ion-col>
-        <ion-col>
-          <button (click)="changeFontSize('medium')" ion-item detail-none class="text-button text-medium">A</button>
-        </ion-col>
-        <ion-col>
-          <button (click)="changeFontSize('large')" ion-item detail-none class="text-button text-larger">A</button>
-        </ion-col>
-      </ion-row>
-      <ion-row class="row-dots">
-        <ion-col>
-          <button (click)="changeBackground('white')" category="dot" class="dot-white" [class.selected]="background == 'white'"></button>
-        </ion-col>
-        <ion-col>
-          <button (click)="changeBackground('tan')" category="dot" class="dot-tan" [class.selected]="background == 'tan'"></button>
-        </ion-col>
-        <ion-col>
-          <button (click)="changeBackground('grey')" category="dot" class="dot-grey" [class.selected]="background == 'grey'"></button>
-        </ion-col>
-        <ion-col>
-          <button (click)="changeBackground('black')" category="dot" class="dot-black" [class.selected]="background == 'black'"></button>
-        </ion-col>
-      </ion-row>
-      <ion-item class="text-default">
-        <ion-label>{{default.name}}</ion-label>
-        <ion-radio value="{{default.value}}"></ion-radio>
-      </ion-item>
-      <ion-item class="text-athelas">
-        <ion-label>Athelas</ion-label>
-        <ion-radio value="Athelas"></ion-radio>
-      </ion-item>
-      <ion-item class="text-charter">
-        <ion-label>Charter</ion-label>
-        <ion-radio value="Charter"></ion-radio>
-      </ion-item>
-      <ion-item class="text-iowan">
-        <ion-label>Iowan</ion-label>
-        <ion-radio value="Iowan"></ion-radio>
-      </ion-item>
-      <ion-item class="text-palatino">
-        <ion-label>Palatino</ion-label>
-        <ion-radio value="Palatino"></ion-radio>
-      </ion-item>
-      <ion-item class="text-san-francisco">
-        <ion-label>San Francisco</ion-label>
-        <ion-radio value="San Francisco"></ion-radio>
-      </ion-item>
-      <ion-item class="text-seravek">
-        <ion-label>Seravek</ion-label>
-        <ion-radio value="Seravek"></ion-radio>
-      </ion-item>
-      <ion-item class="text-times-new-roman">
-        <ion-label>Times New Roman</ion-label>
-        <ion-radio value="Times New Roman"></ion-radio>
-      </ion-item>
-    </ion-list>
-  `,
+  templateUrl: 'build/components/PopoverChapterReader.html',
 })
 export class PopoverChapterReader {
   background: string;
