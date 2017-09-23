05dc281c5e0bb0b6465ae16807cdbb13eb130508
AzSiAz <slider.perso@live.fr>
2017-09-24 00:57:41 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View
+} from 'react-native';
+
+export default class LNReader extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('LNReader', () => LNReader);
