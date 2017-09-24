d378296b887c7e1aa226d9d5a0bd87f11287e84c
AzSiAz <slider.perso@live.fr>
2017-09-24 20:20:56 +0200

    add deps

--- android/app/src/main/java/com/lnreader/MainApplication.java
+++ android/app/src/main/java/com/lnreader/MainApplication.java
@@ -3,6 +3,7 @@ package com.lnreader;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
             new RealmReactPackage(),
             new VectorIconsPackage()
       );
